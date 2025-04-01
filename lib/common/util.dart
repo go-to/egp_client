937fb5d6e07d9c064fa9d70043cee88689e1ddd1
go-to <go8ogle@gmail.com>
2025-04-01 19:40:34 +0900

    ダイアログ表示用の共通関数を定義

--- lib/common/util.dart
+++ lib/common/util.dart
@@ -1,8 +1,54 @@
+import 'package:flutter/material.dart';
+
 class Util {
   static String sprintf(String template, List<dynamic> values) {
     for (var value in values) {
       template = template.replaceFirst(RegExp(r'%s|%d|%f'), value.toString());
     }
     return template;
+  }
+
+  static void showAlertDialog(
+      BuildContext context, String text, String buttonLabel,
+      [Function? func]) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SizedBox(
+            height: 50,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(text,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black)),
+              ],
+            ),
+          ),
+          actions: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  if (func != null) {
+                    func();
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(255, 255, 255, 0.9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(buttonLabel,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black)),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
