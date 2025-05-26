3259c28897e7d6d121f19eb8856006e7bd9f2d3b
go-to <go8ogle@gmail.com>
2025-05-26 12:02:43 +0900

    設定画面のテーマ設定をCard化

--- lib/view/setting_page.dart
+++ lib/view/setting_page.dart
@@ -11,6 +11,8 @@ class SettingPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentTheme = ref.watch(themeNotifierProvider).value;
+    // 現在のテーマからカラースキームを取得
+    final colorScheme = Theme.of(context).colorScheme;
 
     return PopScope(
       canPop: false,
@@ -19,28 +21,60 @@ class SettingPage extends ConsumerWidget {
       },
       child: Scaffold(
         appBar: AppBar(title: const Text(Config.settingPageTitle)),
-        body: Column(
+        body: ListView(
+          padding: EdgeInsets.all(16),
           children: [
-            RadioListTile<ThemeMode>(
-              title: const Text(Config.themeLight),
-              value: ThemeMode.light,
-              groupValue: currentTheme,
-              onChanged: (mode) =>
-                  ref.read(themeNotifierProvider.notifier).updateTheme(mode!),
-            ),
-            RadioListTile<ThemeMode>(
-              title: const Text(Config.themeDark),
-              value: ThemeMode.dark,
-              groupValue: currentTheme,
-              onChanged: (mode) =>
-                  ref.read(themeNotifierProvider.notifier).updateTheme(mode!),
-            ),
-            RadioListTile<ThemeMode>(
-              title: const Text(Config.themeSystemSetting),
-              value: ThemeMode.system,
-              groupValue: currentTheme,
-              onChanged: (mode) =>
-                  ref.read(themeNotifierProvider.notifier).updateTheme(mode!),
+            // アカウント設定
+            // Card(),
+            // SizedBox(height: 16),
+            // テーマ設定
+            Card(
+              shadowColor: colorScheme.primary,
+              surfaceTintColor: colorScheme.primary.withAlpha(0),
+              elevation: 4,
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.color_lens, color: colorScheme.primary),
+                        SizedBox(width: 8),
+                        Text(Config.settingItemTheme,
+                            style: TextStyle(
+                                fontSize: Config.fontSizeMediumLargeMiddle,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    RadioListTile<ThemeMode>(
+                      title: const Text(Config.themeLight),
+                      value: ThemeMode.light,
+                      groupValue: currentTheme,
+                      onChanged: (mode) => ref
+                          .read(themeNotifierProvider.notifier)
+                          .updateTheme(mode!),
+                    ),
+                    RadioListTile<ThemeMode>(
+                      title: const Text(Config.themeDark),
+                      value: ThemeMode.dark,
+                      groupValue: currentTheme,
+                      onChanged: (mode) => ref
+                          .read(themeNotifierProvider.notifier)
+                          .updateTheme(mode!),
+                    ),
+                    RadioListTile<ThemeMode>(
+                      title: const Text(Config.themeSystemSetting),
+                      value: ThemeMode.system,
+                      groupValue: currentTheme,
+                      onChanged: (mode) => ref
+                          .read(themeNotifierProvider.notifier)
+                          .updateTheme(mode!),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
